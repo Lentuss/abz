a4c2e2458cd96dcfa843db96e4fc8996290f2c61
Olena Pohybil <itlentus@gmail.com>
2023-01-26 11:15:12 +0200

    test

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { store } from 'redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
     <Provider store={store}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/abz">
           <App />
         </BrowserRouter>   
       </Provider>
